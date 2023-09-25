ce92efb186153c8e80faa7478ea2dd950fa1d10e
Marcin Szamotulski <coot@coot.me>
2023-09-25 21:36:14 +0200

    extended: lsp servers

--- init.lua
+++ init.lua
@@ -452,7 +452,9 @@ end
 --
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
-local servers = {
+local servers
+if extended then
+servers = {
   html = { filetypes = { 'html', 'twig', 'hbs'} },
   lua_ls = {
     Lua = {
@@ -461,6 +463,16 @@ local servers = {
     },
   },
 }
+else
+servers = {
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+}
+end
 
 -- Setup neovim lua configuration
 require('neodev').setup()
