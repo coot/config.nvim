4644775482fe679ef5060f101d4a70de199d7511
Marcin Szamotulski <coot@coot.me>
2023-10-20 08:42:18 +0200

    Removed duplicated map

--- lua/custom/plugins/maps.lua
+++ lua/custom/plugins/maps.lua
@@ -15,7 +15,6 @@ vim.keymap.set('n', '<F3>', function()
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>ol', require('telescope.builtin').oldfiles, { desc = '[O]ld [F]iles' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
